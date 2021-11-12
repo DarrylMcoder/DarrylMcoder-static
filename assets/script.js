d443d5682ffce7e071299cf30a65a815fe96a714
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-11-12 17:41:11 -0500

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -7,8 +7,26 @@ function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
 
-$(function() {
-  $(".morebtn").click(function() {
-    $(this).siblings( ".morecontent" ).slideDown( "fast" );
-  });
-});
+function decrypt(crypted,key) {
+          alert("crypted:  "+crypted);
+        var alpha = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890";
+        var decrypted_str = "";
+        var found = false;
+        for(var i = 0; i < crypted.length; i++) {
+          var crypted_val = crypted.charAt(i);
+          for(var j = 0; j < key.length; j++){
+            var key_val = key.charAt(j);
+            var alpha_val = alpha.charAt(j);
+            if(key_val == crypted_val) {
+              decrypted_str += alpha_val;
+              found = true;
+            }
+          }
+          if(found != true) {
+            decrypted_str += crypted_val;
+          }
+          found = false;
+        }
+        alert("decrypted:   "+decrypted_str);
+        return decrypted_str;
+      }
